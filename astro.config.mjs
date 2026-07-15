40119d3e94778107344b50a034c961b65ab2093a
malchenkovasvetlana-eng <malchenkovasvetlana@gmail.com>
2026-07-15 17:29:44 +0300

    Publish Tatiana portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   output: "static",
   site: "https://malchenkovasvetlana-eng.github.io",
-  base: "/tatyana-krasavtseva",
+  base: "/tatiana-krasavtseva",
 });
